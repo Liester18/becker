794fd32556a4173c648c7a2153a40c7db0683e1e
aluno <aluno@HAVANA>
2016-03-26 19:07:55 -0300

    Cambios en tabla y filtros: -Retorno variable para el lado cliente. -Presentacion, inicio internacionalizacion. -Eliminacion responsables repetidos.

--- src/ReportsBundle/Controller/DefaultController.php
+++ src/ReportsBundle/Controller/DefaultController.php
@@ -18,7 +18,7 @@ class DefaultController extends Controller {
         $em = $this->getDoctrine()->getEntityManager();
         $db = $em->getConnection();
  
-        $query = "SELECT paciente.nome,responsavel.nome as nome_resp,orden_responsavel.orden 
+        $query = "SELECT paciente.nome,responsavel.nome as nome_resp, orden_responsavel.paciente_id, orden_responsavel.responsavel_id, orden_responsavel.orden   
             FROM public.orden_responsavel, public.responsavel, public.paciente 
             WHERE orden_responsavel.paciente_id = paciente.id_paciente AND
             orden_responsavel.id = responsavel.id_responsavel;"; //limit 10
@@ -26,19 +26,28 @@ class DefaultController extends Controller {
         $params = array();
         $stmt->execute($params);
         $po=$stmt->fetchAll();
- 
-        // Mostrar todo
-//        foreach ($po as $p) {
-//            echo $p["nome"];
-//            echo "<br/>";
-//            echo $p["nome_resp"];
-//            echo "<hr/>";
-//            echo $p["orden"];
-//            echo "<hr/>";
-//        }
-       // var_dump($po); die();
         
-        return $this->render('ReportsBundle:Default:dashboard.html.twig', array('last_username' => $sesion->get(SecurityContext::LAST_USERNAME), 'error' => $error, 'p'=> $po));
+        $final = array();
+        $p = 0;
+        $p_ant = 0;
+        foreach ($po as $v) {          
+            
+            if($p_ant == $v["paciente_id"]){
+                $final[$p-1]["responsavel_id"] = $v["responsavel_id"];
+                $final[$p-1]["nome_resp2"] = $v["nome_resp"];                
+                $final[$p-1]["orden"] = $v["orden"];                
+            }else{
+                $final[$p]["paciente_id"] = $v["paciente_id"];                        
+                $final[$p]["nome"] = $v["nome"];
+                $final[$p]["responsavel_id"] = $v["responsavel_id"];
+                $final[$p]["nome_resp"] = $v["nome_resp"];
+                $final[$p]["orden"] = $v["orden"];
+            }
+            $p++;
+            $p_ant = $v["paciente_id"];
+        }     
+        
+        return $this->render('ReportsBundle:Default:dashboard.html.twig', array('last_username' => $sesion->get(SecurityContext::LAST_USERNAME), 'error' => $error, 'p'=> $final));
     }
     
 
