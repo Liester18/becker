922159c667470acbe0f0016563384ebd50652d57
aluno <aluno@HAVANA>
2016-03-27 15:05:40 -0300

    Cambios en tabla: -Internacionalizacion. -Apagar elementos por linea.

--- src/ReportsBundle/Controller/DefaultController.php
+++ src/ReportsBundle/Controller/DefaultController.php
@@ -18,7 +18,7 @@ class DefaultController extends Controller {
         $em = $this->getDoctrine()->getEntityManager();
         $db = $em->getConnection();
  
-        $query = "SELECT paciente.nome,responsavel.nome as nome_resp, orden_responsavel.paciente_id, orden_responsavel.responsavel_id, orden_responsavel.orden   
+        $query = "SELECT paciente.nome,responsavel.nome as nome_resp, orden_responsavel.paciente_id, orden_responsavel.responsavel_id   
             FROM public.orden_responsavel, public.responsavel, public.paciente 
             WHERE orden_responsavel.paciente_id = paciente.id_paciente AND
             orden_responsavel.id = responsavel.id_responsavel;"; //limit 10
@@ -34,20 +34,31 @@ class DefaultController extends Controller {
             
             if($p_ant == $v["paciente_id"]){
                 $final[$p-1]["responsavel_id"] = $v["responsavel_id"];
-                $final[$p-1]["nome_resp2"] = $v["nome_resp"];                
-                $final[$p-1]["orden"] = $v["orden"];                
+                $final[$p-1]["nome_resp2"] = $v["nome_resp"];                               
             }else{
                 $final[$p]["paciente_id"] = $v["paciente_id"];                        
                 $final[$p]["nome"] = $v["nome"];
                 $final[$p]["responsavel_id"] = $v["responsavel_id"];
                 $final[$p]["nome_resp"] = $v["nome_resp"];
-                $final[$p]["orden"] = $v["orden"];
             }
             $p++;
             $p_ant = $v["paciente_id"];
         }     
         
         return $this->render('ReportsBundle:Default:dashboard.html.twig', array('last_username' => $sesion->get(SecurityContext::LAST_USERNAME), 'error' => $error, 'p'=> $final));
+    }
+    
+    // Para cargar solo el body de la pagina
+    public function deleteJSAction() {              
+        $request = $this->getRequest();
+        $id = $request->get('id');           
+        
+        $em = $this->getDoctrine()->getManager();
+        $paciente = $em->getRepository('EntityBundle:Paciente')->findOneBy(Array('idPaciente' => $id));
+        $em->remove($paciente);
+        $em->flush();
+        
+         return new \Symfony\Component\HttpFoundation\Response(json_encode('OK'), 200, array('Content-Type' => 'application/json'));
     }
     
 
