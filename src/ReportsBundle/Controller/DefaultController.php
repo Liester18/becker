ca16027954da5720af64ed1bd6cdb5153b8cbf34
aluno <aluno@HAVANA>
2016-04-01 18:43:51 -0300

    Variante servidor para .csv

--- src/ReportsBundle/Controller/DefaultController.php
+++ src/ReportsBundle/Controller/DefaultController.php
@@ -5,6 +5,9 @@ namespace ReportsBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\SecurityContext;
 use EntityBundle\Entity;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class DefaultController extends Controller {
     
@@ -18,7 +21,7 @@ class DefaultController extends Controller {
         $em = $this->getDoctrine()->getEntityManager();
         $db = $em->getConnection();
  
-        $query = "SELECT paciente.nome,responsavel.nome as nome_resp, orden_responsavel.paciente_id, orden_responsavel.responsavel_id   
+        $query = "SELECT paciente.nome,responsavel.nome as nome_resp, responsavel.email, orden_responsavel.paciente_id, orden_responsavel.responsavel_id   
             FROM public.orden_responsavel, public.responsavel, public.paciente 
             WHERE orden_responsavel.paciente_id = paciente.id_paciente AND
             orden_responsavel.id = responsavel.id_responsavel;"; //limit 10
@@ -34,12 +37,14 @@ class DefaultController extends Controller {
             
             if($p_ant == $v["paciente_id"]){
                 $final[$p-1]["responsavel_id"] = $v["responsavel_id"];
-                $final[$p-1]["nome_resp2"] = $v["nome_resp"];                               
+                $final[$p-1]["nome_resp2"] = $v["nome_resp"];
+                $final[$p-1]["email_resp2"] = $v["email"];
             }else{
                 $final[$p]["paciente_id"] = $v["paciente_id"];                        
                 $final[$p]["nome"] = $v["nome"];
                 $final[$p]["responsavel_id"] = $v["responsavel_id"];
                 $final[$p]["nome_resp"] = $v["nome_resp"];
+                $final[$p]["email_resp"] = $v["email"];                
             }
             $p++;
             $p_ant = $v["paciente_id"];
@@ -59,6 +64,27 @@ class DefaultController extends Controller {
         $em->flush();
         
          return new \Symfony\Component\HttpFoundation\Response(json_encode('OK'), 200, array('Content-Type' => 'application/json'));
+    }
+    
+    public function expAction(){
+        $request = $this->getRequest();
+        $response = new StreamedResponse(function() use($request) {            
+            $responsables = $request->get('responsable');
+            $email = $request->get('email');
+    //        var_dump($responsable);
+    //        var_dump($email);
+    //        die();  
+            $handle = fopen('php://output', 'r+');
+            fputs($handle, implode(Array('Email', 'Nome'), ';')."\n");
+            foreach ($responsables as $k => $v) {
+                fputs($handle, implode(Array($email[$k], $v), ';')."\n");
+            }             
+            fclose($handle);
+        });
+        $response->headers->set('Content-Type', 'application/force-download');
+        $response->headers->set('Content-Disposition', 'attachment; filename="export.csv"');
+
+        return $response;
     }
     
 
