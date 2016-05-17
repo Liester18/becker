7b4b581ea831d0e8f638b8e2a1cb2882cf07c6e6
liester <liester@192.168.56.1>
2016-05-17 15:21:12 -0300

    validaciones comenzando a entender...

--- src/DailyWorkBundle/Form/Responsavel_F/ResponsavelType.php
+++ src/DailyWorkBundle/Form/Responsavel_F/ResponsavelType.php
@@ -28,7 +28,7 @@ class ResponsavelType extends AbstractType{
                 ->add('nome', 'text')
                 ->add('ocupacao')
                 ->add('telefContato', 'text',  array('label' => 'Telef de Contato'))
-                ->add('email')
+                ->add('email', 'email')
                 ->add('endereco', new EnderecoType(), array(
                     'label' => false,
                     'by_reference' => false,
