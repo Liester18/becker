1af6df8a680e4f1b72fa9aa74b972d210cad7506
julio <julio@julio-ThinkPad>
2016-06-12 14:21:15 -0300

    Arreglando cosas del idioma.

--- src/DailyWorkBundle/Form/Paciente/PacienteType.php
+++ src/DailyWorkBundle/Form/Paciente/PacienteType.php
@@ -40,53 +40,34 @@ class PacienteType extends AbstractType {
         $builder
         ->add('nome', 'text', array('max_length' => 100, 'label' => 'Nome'))
         ->add('data_nascimento', 'date', array('required' => false, 'widget' => 'single_text', 'format' => 'dd/MM/yyyy'))
-        ->add('tipoSanguineo', 'entity', array('class' => 'EntityBundle:TipoSanguineo', 'multiple' => false, 'required' => false))
+        ->add('tipoSanguineo', 'entity', array('class' => 'EntityBundle:TipoSanguineo', 'multiple' => false, 'required' => false,
+                    'placeholder' => 'SELECIONAR'))
         ->add('problemas_gravidez', 'textarea', array('required' => false))
         ->add('leite_artificial', 'checkbox', array('required' => false))
         ->add('depressao_puerperal', 'textarea', array('required' => false))
         ->add('problemas_posparto', 'textarea', array('required' => false))
-        ->add('leite_materno_exclusivo', 'checkbox', array('required' => false))
-        ->add('leite_materno_idade', 'number', array('required' => false))
-        ->add('sex', 'choice', array('label'=> 'Sexo',
+        ->add('leite_materno_exclusivo', 'choice', array(
                     'choices' => array(
-                    0 => 'M',
-                    1 => 'F'
-                ),
-                'data' => 1
-            ))
+                        'SELECIONAR' => 'placeholder',
+                        'Artificial' => false,
+                        'Materno' => true,
+                    ),
+                    // *this line is important*
+                    'choices_as_values' => true,
+                        //'required' => false,
+                ))
+        ->add('leite_materno_idade', 'number', array('required' => false))
+        ->add('sex', 'choice', array('label' => 'Sexo',
+                    'choices' => array(
+                        'SELECIONAR' => 'placeholder',
+                        'M' => false,
+                        'F' => true,
+                    ),
+                    //'data' => 1,
+                    'choices_as_values' => true,
+                ))
         ->add('active', 'checkbox', array('label'=> 'Ativo'))
-        //->add('ntipoparto')
-        //->add('ntiposanguineo')
-        /*->add('endereco', new Endereco\EnderecoType(), array(
-        'label' => 'CEP',
-        'by_reference' => false,
-        'required' => false
-        ))*/
-        //->add('endereco', 'collection', array('type' => new Endereco\EnderecoType()))
-        /* ->add('responsavel', new Responsavel_F\ResponsavelType(), array(
-          //'label'          => 'Direcciones',
-          'by_reference'   => false,
-          'required'=> false
-          ))
-          /*->add('responsavel', new Responsavel_F\ResponsavelType(), array(
-          //'label'          => 'Direcciones',
-          'by_reference'   => false,
-          'required'=> false
-          )) */
-        /* ->add('responsavels', 'collection', array(
-          'type'           => new Responsavel_F\ResponsavelType(),
-          //'label'          => 'Responsavels',
-          'by_reference'   => false,
-          'data'           => $this->responsavels,
-          'mapped'         => false,
-          'required'       => false
-          //'prototype_data' => new \EntityBundle\Entity\Responsavel()
-          //'allow_delete'   => true,
-          //'allow_add'      => true,
-          //'attr'           => array(
-          //   'class' => 'row addresses'
-          //)
-          )) */
+        
         ->add('responsavels', 'collection', array(
             'type'  => new Responsavel_F\ResponsavelType(),
             'label' => false,
@@ -95,17 +76,9 @@ class PacienteType extends AbstractType {
             
            // 'data' => $this->responsavels
         ))
-        /* ->add('ordenresponsavels', ''new Responsavel_F\ResponsavelType(), array(
-          //'label'          => 'Direcciones',
-          'by_reference'   => false,
-          'required'=> false
-          )) */
-        /* ->add('tipoParto', new TipoParto\TipoPartoType(), array(
-          'label'          => 'Tipo de parto',
-          'by_reference'   => false,
-          'required'=> false
-          )) */
-        ->add('tipoParto', 'entity', array('class' => 'EntityBundle:TipoParto', 'multiple' => false, 'required' => false ))
+       
+       ->add('tipoParto', 'entity', array('class' => 'EntityBundle:TipoParto', 'multiple' => false, 'required' => false,
+                     'placeholder' => 'SELECIONAR', 'label'=> 'Tipo parto'))
         
         ->add('paciente_problemasSaude', 'collection', array(
             'type'  => new ProblemasSaude\Paciente_ProblemasSaudeType(),
